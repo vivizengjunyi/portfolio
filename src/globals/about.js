2ca9b4e447ba2dc7354bb544c46facd9d73aa3c3
Vivi <vivizengjunyi@gmail.com>
2024-04-04 18:20:13 -0700

    improve

--- src/globals/about.js
+++ src/globals/about.js
@@ -5,7 +5,7 @@ const about = [
     {
         'summary': [
             "Versatile in using a wide array of front-end technologies and frameworks, such as React.js, TypeScript, Redux, JavaScript, Tailwind CSS, and Bootstrap.",
-            "Expert in the integration and maintenance of API endpoints to ensure dynamic content is consistently updated, coupled with a proficiency in using Google DevTools for debugging.",
+            "Expert in utilizing API endpoints for dynamic content updating and debugging using Google DevTools.",
             "Proficient in applying Jest for comprehensive unit and integration testing within React projects.",
             "Possesses a thorough understanding of UX and design principles, always aiming to improve user engagement and satisfaction through their application.",
             "Known for excellent communication skills, an unwavering drive, and a dedication to fostering teamwork."]
